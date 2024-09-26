a4b0a8f3a60666b7ac1e6624e43730f83abb79f5
Manuel Alarcon <alarmanuel16@gmail.com>
2024-09-26 10:16:47 +0200

    Pop up login finished

--- js/login.js
+++ js/login.js
@@ -1,7 +1,27 @@
 
 function login() {
 
-    
+    let user ;
+    let pwrd ;
+    let retry ;
+
+    user = prompt("Enter your user");
+    pwrd = prompt("Enter your password");
+
+
+    if((user!=="Manuel")||(pwrd!=="dejapaso")){
+        alert("Incorrect credentials.") ;
+        retry = confirm("Would you like to try again?") ;
+
+        if(retry === true){
+            login() ;
+        }
+
+    } else {
+
+        document.getElementById("mainContent").style.display = "block";
+
+    }
 
 }
 
