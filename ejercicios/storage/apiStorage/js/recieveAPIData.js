567641b7c0edd73377e6643134c97e5f3a8dfdc5
Manuel Alarcon <alarmanuel16@gmail.com>
2024-11-14 13:27:54 +0100

    PokeAPI Shiny Update

--- ejercicios/storage/apiStorage/js/recieveAPIData.js
+++ ejercicios/storage/apiStorage/js/recieveAPIData.js
@@ -14,13 +14,15 @@ async function recieveAPIData(id){
 
     const data = await poke.json();
 
+    let shiny = randomNum(0,40) ;
+
     const elements = {
 
         pokedexNum: data.id,
         name: firstUpper(data.name),
         type1: firstUpper(data.types[0].type.name),
         type2: firstUpper((data.types[1]!=null)?data.types[1].type.name:"------"),
-        sprite: data.sprites.front_default
+        sprite: (shiny==0)? data.sprites.front_shiny : data.sprites.front_default
 
     } ;
 
